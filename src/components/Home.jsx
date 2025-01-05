1ef2c65314750d390f556670137203c67420a849
shovonroy173 <shovonroy2003@gmail.com>
2025-01-05 15:59:31 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import MultiCardCarousel from "./Carousel";
+
+const Home = () => {
+  let slides = [
+    "https://i.pinimg.com/originals/51/82/ac/5182ac536727d576c78a9320ac62de30.jpg",
+    "https://wallpapercave.com/wp/wp3386769.jpg",
+    "https://wallpaperaccess.com/full/809523.jpg",
+    "https://getwallpapers.com/wallpaper/full/5/c/0/606489.jpg",
+  ];
+  return (
+    <div>
+      <div className="w-[60%] m-auto pt-11">
+      <MultiCardCarousel slides={slides} />
+    </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/dummyData.js
@@ -0,0 +1,9 @@
+export const navLinks = [
+    {id:1 , title:"Home" , url:"/"} , 
+    {id:2 , title:"About" , url:"/about"} , 
+    {id:1 , title:"Portfolio" , url:"/portfolio"} , 
+    {id:1 , title:"Clients" , url:"/clients"} , 
+    {id:1 , title:"Blog" , url:"/blog"} , 
+    {id:1 , title:"Contact" , url:"/contact"} , 
+];
+
